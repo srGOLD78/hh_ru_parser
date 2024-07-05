0c77c1d432cb86bb0b3f45be6bfbd1d4f481e499
srGOLD78 <145363900+srGOLD78@users.noreply.github.com>
2024-07-05 14:49:57 +0300

    Update db.py

--- db.py
+++ db.py
@@ -167,17 +167,6 @@ def calculate_average_salary_candidates():
 
         average_salary = total_salary / count
         return average_salary
-
-def clear_vacancies():
-    with sqlite3.connect('my_database.db') as connection:
-        cursor = connection.cursor()
-        cursor.execute("DELETE FROM vacancies")
-        connection.commit()
-def clear_candidates():
-    with sqlite3.connect('my_database.db') as connection:
-        cursor = connection.cursor()
-        cursor.execute("DELETE FROM candidates")
-        connection.commit()
         
 def remove_duplicates_vacancies():
     with sqlite3.connect('my_database.db') as connection:
